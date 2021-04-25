041ca83728896c353fb959a6180b790bad53932e
Persovt <mza200339@gmail.com>
2021-04-25 12:46:14 +0300

    WOWOWOWOOWOWOWOWO lol(:#:#@:#:@

--- script.js
+++ script.js
@@ -24,56 +24,79 @@ const ProgressBar = require("progress");
 
   const cookies = await page.cookies();
 
-  const getUserPages = await browser.newPage();
-  await getUserPages.goto(
-    "https://lk.webheroschool.ru/pl/user/user/index?page=99999999&per-page=30"
+  const getLessonsLength = await browser.newPage();
+  await getLessonsLength.goto(
+    "https://lk.webheroschool.ru/teach/control/stream/tree"
   );
-  let maxPage = await getUserPages.evaluate(() => {
-    return document.querySelector(
-      "#w7 > div > div.panel-footer > div.kv-panel-pager > ul > li.active > a"
-    ).innerHTML;
+  let maxLessons = await getLessonsLength.evaluate(() => {
+    return document.querySelectorAll(".dd-item").length;
   });
 
   const page2 = await browser.newPage();
 
   page2.setCookie(...cookies);
 
-  let content = JSON.parse(fs.readFileSync("user.json", "utf8"));
-  let bar = new ProgressBar("[:bar] :percent :etas", { total: +maxPage });
+  //let content = JSON.parse(fs.readFileSync("lessons.json", "utf8"));
 
-  for (let i = 1; i <= maxPage; i++) {
-    bar.tick();
+  await page2.goto(`https://lk.webheroschool.ru/teach/control/stream/tree`);
+  let bar = new ProgressBar("[:bar] :percent :etas", { total: +maxLessons });
+  bar.tick();
 
-    await page2.goto(
-      `https://lk.webheroschool.ru/pl/user/user/index?page=${i}&per-page=30`
+  let lessons = await page2.evaluate(() => {
+    let lessonsList = {};
+
+    document.querySelector(
+      "body > div.gc-main-content.gc-both-main-content.wide.account-page-content.with-left-menu.gc-user-logined.gc-user-admin > div > div > div > div.row > div:nth-child(1) > div > ol > li:nth-child(2) > ol > li:nth-child(3) > ol > li:nth-child(1)"
     );
-
-    let usersInfo = await page2.evaluate(() => {
-      let pageUserObject = {
-        users: [],
-      };
-      document
-        .querySelectorAll("#w7-container > table > tbody > tr")
-        .forEach((item) => {
-          pageUserObject["users"].push({
-            name: item.querySelector("td.user-name > a").innerHTML,
-            url: item.querySelector("td.user-name > a").href,
-            email: item.querySelector("td:nth-child(3) > a > div").innerText,
-            role: item.querySelector("td:nth-child(4)").innerHTML,
+    document
+      .querySelectorAll(
+        "body > div.gc-main-content.gc-both-main-content.wide.account-page-content.with-left-menu.gc-user-logined.gc-user-admin > div > div > div > div.row > div:nth-child(1) > div > ol > li"
+      )
+      .forEach((level1) => {
+        lessonsList[level1.dataset.id] = {
+          title: level1.querySelector("span a").innerHTML,
+          url: level1.querySelector("span a").href,
+        };
+       
+          level1.querySelectorAll("ol > li").forEach((level2) => {
+            lessonsList[level1.dataset.id][level2.dataset.id] = {
+              title: level1.querySelector("span a").innerHTML,
+              url: level1.querySelector("span a").href,
+              level2: {
+                title: level2.querySelector("span a").innerHTML,
+                url: level2.querySelector("span a").href,
+              },
+            };
+          
+              level2.querySelectorAll("ol > li").forEach((level3) => {
+                lessonsList[level1.dataset.id][level2.dataset.id][
+                  level3.dataset.id
+                ] = {
+                  title: level1.querySelector("span a").innerHTML,
+                  url: level1.querySelector("span a").href,
+                  level2: {
+                    title: level2.querySelector("span a").innerHTML,
+                    url: level2.querySelector("span a").href,
+                    level3: {
+                      title: level3.querySelector("span a").innerHTML,
+                      url: level3.querySelector("span a").href,
+                    },
+                  },
+                };
+              });
           });
-        });
-      return pageUserObject;
-    });
-    content[i] = usersInfo.users;
+      });
 
-    await fs.writeFile(
-      "user.json",
-      JSON.stringify(content, null, 2),
-      (err, data) => {
-        if (err) process.exit();
-      }
-    );
-  }
+    return lessonsList;
+  });
+
+  await fs.writeFile(
+    "lessons.json",
+    JSON.stringify(lessons, null, 2),
+    (err, data) => {
+      if (err) process.exit();
+    }
+  );
 
   await browser.close();
 })();
